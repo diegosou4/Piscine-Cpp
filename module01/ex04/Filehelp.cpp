d3536a736744862d2cf263a2d8d72f0f4aed8c88
diegosou4 <diegoaguia31@gmail.com>
2024-07-03 23:34:53 +0100

    (update) Done

--- module01/ex04/Filehelp.cpp
+++ module01/ex04/Filehelp.cpp
@@ -1,10 +1,5 @@
 #include "replace.h"
-
-
 #include <fstream> 
-
-
-
 
 void read_file(std::ifstream &myfile, std::ofstream &temp, const std::string &str1, const std::string &str2)
 {
@@ -22,34 +17,50 @@ void read_file(std::ifstream &myfile, std::ofstream &temp, const std::string &st
         temp << line << std::endl;
     }
 }
+bool create_temp(std::string file, std::ofstream &temp)
+{
+    std::string replace;
+    replace = file + ".replace";
+    temp.open(replace.c_str());
+    if(!temp.is_open())
+    {
+        std::cout << "Error when we tried to create file temp" << std::endl;
+        return(false);
+    }
+    return(true);
+}
+
+bool open_file(std::ifstream &myfile, std::string &file)
+{
+    myfile.open(file.c_str());
+    if(!myfile.is_open())
+    {  
+        std::cout << "Error when we tried to open: " << file << std::endl;
+        return(false);
+    }
+    return(true);
+}
 
 int init_program(std::string file,std::string str1,std::string str2)
 {
     std::ifstream myfile;
     std::ofstream temp;
     
-    myfile.open(file.c_str());
-
-    if(!myfile.is_open())
-    {  
-        std::cout << "Error when we tried to open: " << file << std::endl;
-        return(0);
-    }
-
-    temp.open("temp");
-    if(!temp.is_open())
+    if(str1.empty())
     {
-        std::cout << "Error when we tried to create file temp" << std::endl;
-        myfile.close();
+        std::cout << "Search string cannot be empty" << std::endl;
         return(0);
     }
-    std::cout << "chega aqui " << std::endl;
+    if(open_file(myfile,file) == false)
+        return(0);
+    if(create_temp(file,temp) == false)
+    {
+          myfile.close();
+          return(0);
+    }
     read_file(myfile,temp,str1,str2);
-
-
     temp.close();
     myfile.close();
-
     return(1);
 }
 
